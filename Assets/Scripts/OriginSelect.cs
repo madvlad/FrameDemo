4eaeecac63476417d397b81b0df85aba61f21eb3
David Bohan <dbohan@apterainc.com>
2016-12-17 11:55:05 -0500

    Initial commit

---
+++ Assets/Scripts/OriginSelect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OriginSelect : MonoBehaviour {
+    public GameObject BuildingFrame;
+
+	// Use this for initialization
+	void Start () {
+        var newBuilding = Instantiate(BuildingFrame);
+
+        // Show origin select prefab and wait for that to be placed, then place the BuildingFrame object
+	}
+}
---
+++ Assets/Scripts/SelectableComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectableComponent : MonoBehaviour {
+
+    private string ComponentName;
+    private string ComponentDimensions;
+    private string ComponentWeight;
+    private int ComponentColor;
+
+	void Start ()
+    {
+        ComponentName = "Wooden Joist";
+        ComponentDimensions = "3' x 3' x 9'";
+        ComponentWeight = "50lb";
+        ComponentColor = 0;
+	}
+	
+    void OnSelect()
+    {
+        // Show billboard in the direction of player and send object reference
+        Debug.Log(ComponentName + " Selected");
+    }
+}
