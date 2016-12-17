c92df0de5c9fca3e055260a3acfff75aea98435e
David Bohan <dbohan@apterainc.com>
2016-12-17 17:15:17 -0500

    Add origin anchor

--- Assets/Scripts/OriginSelect.cs
+++ Assets/Scripts/OriginSelect.cs
@@ -3,11 +3,25 @@ using System.Collections;
 
 public class OriginSelect : MonoBehaviour {
     public GameObject BuildingFrame;
+    public GameObject OriginAnchor;
 
+    private bool isPlacing = true;
 	// Use this for initialization
-	void Start () {
-        var newBuilding = Instantiate(BuildingFrame);
-
-        // Show origin select prefab and wait for that to be placed, then place the BuildingFrame object
+	void Start() {
+        OriginAnchor.SendMessage("OnSelect");
 	}
+
+    void OnSelect()
+    {
+        if (isPlacing)
+        {
+            isPlacing = false;
+        } else
+        {
+            Debug.Log("Origin Set");
+            var frame = Instantiate(BuildingFrame);
+            BuildingFrame.transform.position = OriginAnchor.transform.position;
+            Destroy(OriginAnchor);
+        }
+    }
 }
